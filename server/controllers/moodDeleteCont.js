72ce087fa002a80c7b9e60d51d526e3a75cd42aa
Gaia Iturralde <Iturralde.gaia@gmail.com>
2026-05-04 12:35:56 -0700

    Implemented DELETE endpoint

--- server/controllers/moodDeleteCont.js
+++ server/controllers/moodDeleteCont.js
@@ -1,12 +1,31 @@
-import { readData } from "../data/fileHelper"
+import { readData, writeData } from "../data/fileHelper.js"
 
 export const moodDeleteCont = async(req, res) => {
     try{
-        const data = readData('moods.json')
+        //Reads data
+        const data = await readData('moods.json')
+        console.log('Data loaded:', data)
+        console.log('Request body:', req.body)
 
-        res.status(500).json({
-            success: true,
-            error: data
+        //Find and remove item
+        const itemId = req.body.id
+        console.log('Item ID to delete:', itemId, 'Type:', typeof itemId)
+
+        //Filter out the item
+        let filteredData = data
+
+        data.moods = data.moods.filter(item => {
+            return item.id != itemId
+        })
+
+        //Update json
+        await writeData('moods.json', filteredData)
+
+        //Sends success response
+        res.status(200).json({
+            success:true,
+            message: 'Item deleted successfully',
+            deletedId: itemId
         })
     }catch(err){
         res.status(500).json({
